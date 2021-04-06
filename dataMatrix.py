94ef481dd4a76d01c976fd3a92bd7e56632079d6
tprice1768 <48262710+tprice1768@users.noreply.github.com>
2021-04-06 13:12:56 -0500

    Add files via upload

--- dataMatrix.py
+++ dataMatrix.py
@@ -1,5 +1,6 @@
 import os
 import string
+import PIL
 from PIL import Image
 from pylibdmtx.pylibdmtx import encode
 from pylibdmtx.pylibdmtx import decode
@@ -11,12 +12,17 @@ def createDataMatrix():
     textFile = open("./sample.txt", "r")
     textFileContent = textFile.read()
 
-    splitLength = 60    # number split every n characters
+    splitLength = 40    # number split every n characters
     textFileContentList = [textFileContent[i:i+splitLength] for i in range(0, len(textFileContent), splitLength)]
     textFile.close()
     
-    for i in range(0, len(textFileContentList)):
-        encoded = encode(textFileContentList[i])
+    encoded = encode('hello world')
+    img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
+    img.save('./matricies/dmtx.png')
+    
+#    for i in range(0, len(textFileContentList)):
+#        encoded = encode(textFileContentList[i])
+        
         
 #        img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
 #        img = img.convert("RGBA")
@@ -30,14 +36,14 @@ def createDataMatrix():
 #            else:
 #                newData.append(item)
                 
-        img.putdata(newData)
-        img.save('./matricies/matrix' + str(i) + '.png')
+#        img.putdata(newData)
+#        img.save('./matricies/matrix' + str(i) + '.png')
 
 def readDataMatrix():
 #    for i in range(0, len(textFileContentList)):
 #    decode(Image.open('./matricies/matrix' + str(i) + '.png'))
     
-    print(decode(Image.open('/Users/tateprice 1/Desktop/CSCI 404/steg/matricies/matrix0.png')))
+    print(decode(Image.open('./matricies/dmtx.png')))
 
 createDataMatrix()
 readDataMatrix()
