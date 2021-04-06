9fb90dc9b9a230134488df017ce6d4b71407fdad
tprice1768 <48262710+tprice1768@users.noreply.github.com>
2021-04-06 12:32:58 -0500

    Add files via upload

--- dataMatrix.py
+++ dataMatrix.py
@@ -1,34 +1,43 @@
 import os
-from pylibdmtx.pylibdmtx import encode
-from PIL import Image
-import random
 import string
+from PIL import Image
 from pylibdmtx.pylibdmtx import encode
+from pylibdmtx.pylibdmtx import decode
 
-os.chdir("/Users/tateprice 1/Desktop/CSCI 404/steg")
+def createDataMatrix():
+    
+    os.chdir("/Users/tateprice 1/Desktop/CSCI 404/steg")
 
-textFile = open("./sample.txt", "r")
-textFileContent = textFile.read()
+    textFile = open("./sample.txt", "r")
+    textFileContent = textFile.read()
 
-splitLength = 35    # number split every n characters
-textFileContentList = [textFileContent[i:i+splitLength] for i in range(0, len(textFileContent), splitLength)]
-textFile.close()
+    splitLength = 60    # number split every n characters
+    textFileContentList = [textFileContent[i:i+splitLength] for i in range(0, len(textFileContent), splitLength)]
+    textFile.close()
+    
+    for i in range(0, len(textFileContentList)):
+        encoded = encode(textFileContentList[i])
+        
+#        img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
+#        img = img.convert("RGBA")
 
-#letters = string.ascii_lowercase
+#        datas = img.getdata()
+#
+#        newData = []
+#        for item in datas:
+#            if item[0] == 255 and item[1] == 255 and item[2] == 255:
+#                newData.append((255, 255, 255, 0))
+#            else:
+#                newData.append(item)
+                
+        img.putdata(newData)
+        img.save('./matricies/matrix' + str(i) + '.png')
 
-for i in range(20):
-#    random_data = ''.join(random.choice(letters) for i in range(10))
-    encoded = encode(textFileContentList[i])
-    img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
-    img = img.convert("RGBA")
-    datas = img.getdata()
+def readDataMatrix():
+#    for i in range(0, len(textFileContentList)):
+#    decode(Image.open('./matricies/matrix' + str(i) + '.png'))
+    
+    print(decode(Image.open('/Users/tateprice 1/Desktop/CSCI 404/steg/matricies/matrix0.png')))
 
-    newData = []
-    for item in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:
-            newData.append((255, 255, 255, 0))
-        else:
-            newData.append(item)
-            
-    img.putdata(newData)
-    img.save('/Users/tateprice 1/Desktop/CSCI 404/steg/matricies/test' + str(i) + '.png')
+createDataMatrix()
+readDataMatrix()
