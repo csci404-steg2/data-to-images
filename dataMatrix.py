292ff9508048a3d259665511e3d217cf75075a7b
tprice1768 <48262710+tprice1768@users.noreply.github.com>
2021-04-06 14:43:24 -0500

    Add files via upload

--- dataMatrix.py
+++ dataMatrix.py
@@ -1,15 +1,32 @@
+__author__ = "Dylan Martie, Tate Price"
+__copyright__ = "Copyright 2021, Dylan Martie & Tate Price"
+__credits__ = ["Jack Hance, Benjamin Harvey, Dylan Martie, James Palmer, Tate Price"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Dylan Harvey, Tate Price"
+__email__ = "dylan.martie@ndsu.edu, tate.price@ndsu.edu"
+__status__ = "Production"
+
+# required packages:
+# treepoem 3.9.0
+# ghostscript 0.7
+# pillow 8.2.0
+
 import os
 import string
 import PIL
 from PIL import Image
-from pylibdmtx.pylibdmtx import encode
 from pylibdmtx.pylibdmtx import decode
 import treepoem
 
+# @parameter p: textFile .txt
+# @returns: imageFile .png
 def createDataMatrix():
-    
+
+    # changing working direcotry to work with Tate's system
     os.chdir("/Users/tateprice 1/Desktop/CSCI 404/steg")
 
+    # reading contents of text file
     textFile = open("./sample.txt", "r")
     textFileContent = textFile.read()
 
@@ -17,8 +34,11 @@ def createDataMatrix():
     textFileContentList = [textFileContent[i:i+splitLength] for i in range(0, len(textFileContent), splitLength)]
     textFile.close()
       
-    for i in range(0, len(textFileContentList)):              
+    for i in range(0, len(textFileContentList)):
+        #generating data matrices with treepoem    
         img = treepoem.generate_barcode(barcode_type='datamatrix', data=textFileContentList[i],)
+
+        # removing white pixels from image
         img = img.convert("RGBA")
         datas = img.getdata()
         newData = []
@@ -27,7 +47,8 @@ def createDataMatrix():
                 newData.append((255, 255, 255, 0))
             else:
                 newData.append(item)
-                
+
+        # saving image to file       
         img.putdata(newData)
         img.save('./matricies/matrix' + str(i) + '.png')
 
@@ -36,7 +57,7 @@ def readDataMatrix():
 #    for i in range(0, len(textFileContentList)):
 #    decode(Image.open('./matricies/matrix' + str(i) + '.png'))
     
-    print(decode(Image.open('./matricies/dmtx2.png')))
+    print(decode(Image.open('./matricies/matrix0.png')))
 
 createDataMatrix()
-#readDataMatrix()
+readDataMatrix()
